c29cc8d7365fd524b970c5fbb7950452657d36ac
Owen Thurston <thurston.owen1@gmail.com>
2024-01-13 21:03:59 -0800

    done for the day

--- day3.py
+++ day3.py
@@ -12,16 +12,30 @@ for i, row in enumerate(grid):
         if not (val == '.' or val.isdigit()):
             symbols.append((i, j))
 
-# Search areas adjacent to symbols
-adjacency_masks = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+numbers = []
 
+# Search areas adjacent to symbols
 for symbol in symbols:
 
-    # Check 8 spaces adjacent to symbol using adjacency_mask
-    for mask in adjacency_masks:
-        char = grid[symbol[0] + mask[0]][symbol[1] + mask[1]]
+    # Check 8 spaces adjacent to symbol starting upper left
+    offset = -1
+    num = ''
+    char = grid[symbol[0] -1][symbol[1] + offset]
 
-        if not char == '.' and char.isdigit():
-            # Continue reading numbers if a digit is adjacent to a symbol
+    # Continue reading numbers if a digit is adjacent to a symbol
+    while char.isdigit():
+        num = char + num
+        offset -= 1
+        if symbol[1] + offset >= 0:
+            char = grid[symbol[0] -1][symbol[1] + offset]
+        else:
+            break
+
+    # Rows
+    # TODO: trying to write up a loop approach
+    for i in (-1, 0, 1):
+        for j in (-1, 0, 1):
+            num = ''
+            char = grid[symbol[0] + i][symbol[1] + offset]
 
 # Avoid double checking areas if there are symbols adjacent to each other
