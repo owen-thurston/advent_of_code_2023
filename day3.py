a3b4036c76fb49e2026da43d1e02ca7b61ad4650
Owen Thurston <thurston.owen1@gmail.com>
2024-01-14 18:58:52 -0800

    Adjacency grid mask sucessfully created

--- day3.py
+++ day3.py
@@ -1,3 +1,7 @@
+# Create boolean adjacency grids for symbols and numbers
+# Take ^ of grids to find out which numbers to keep
+from pprint import pprint
+
 # Read input into 2x2 grid
 grid = []
 
@@ -6,25 +10,26 @@ with open('test.txt', 'r') as f:
         grid.append(list(row.strip()))
 
 # Make grid for symbols and numbers
-symbol_grid = []
-number_grid = []
+symbol_grid = [[False for x in range(len(grid[0]))] for x in range(len(grid))]
 
 def mark_adjacent_cells(grid, x, y):
     # If there are adjacent symbols, some cells will be marked extra times
     # Accepted as simplicity trade-off
     for i in (-1, 0, 1):
         for j in (-1, 0, 1):
-            # FIXME: out of bounds errors
+            if x + i < 0 or y + j < 0:
+                # Start of row already reached
+                return
             try:
-                grid[x + i][y + i] = True
-            except IndexError
+                grid[x + i][y + j] = True
+            except IndexError:
+                # End of row already reached
+                return
 
-# Populate grids
+# Populate symbol adjacency grid
 for i, row in enumerate(grid):
     for j, val in enumerate(row):
-        if val == '.':
-            continue
-        elif val.isdigit():
-            mark_adjacent_cells(number_grid, i, j)
-        else:
+        if not (val == '.' or val.isdigit()):
             mark_adjacent_cells(symbol_grid, i, j)
+
+pprint(symbol_grid)
