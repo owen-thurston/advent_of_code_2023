8578f63d51c665f6989c0ee30a0b438e68eddeac
Owen Thurston <thurston.owen1@gmail.com>
2024-01-14 17:04:48 -0800

    Working on boolean mask approach.

--- day3.py
+++ day3.py
@@ -5,47 +5,26 @@ with open('test.txt', 'r') as f:
     for row in f:
         grid.append(list(row.strip()))
 
-# Find symbol coordinates
-symbols = []
-for i, row in enumerate(grid):
-    for j, val in enumerate(row):
-        if not (val == '.' or val.isdigit()):
-            symbols.append((i, j))
+# Make grid for symbols and numbers
+symbol_grid = []
+number_grid = []
 
-numbers = []
-
-# Search areas adjacent to symbols
-for symbol in symbols:
-
-    # Check 8 spaces adjacent to symbol starting upper left
+def mark_adjacent_cells(grid, x, y):
+    # If there are adjacent symbols, some cells will be marked extra times
+    # Accepted as simplicity trade-off
     for i in (-1, 0, 1):
         for j in (-1, 0, 1):
-            num = ''
-            char = grid[symbol[0] + i][symbol[1] + j]
+            # FIXME: out of bounds errors
+            try:
+                grid[x + i][y + i] = True
+            except IndexError
 
-            if i == 0 and j == 0:
-                # This is the symbol coordinates
-                break
-
-            offset = j
-            while char.isdigit() and 0 <= symbol[1] + offset and symbol[1] + offset < len(grid[symbol[0]]):
-                # Find start of number    
-                num = char + num
-                offset -= 1
-                char = grid[symbol[0] + i][symbol[1] + offset]
-
-                # Continue collecting number on right
-                # Avoid double checking if grid space already checked
-                    
-
-
-            #         num = num + char
-            #         offset += 1
-            #         # TODO: don't check j = 1 if both j = 0 and j = 1 are digits
-            #         try:
-            #             char = grid[symbol[0] + i][symbol[1] + offset]
-            #         except IndexError:
-            #             break
-
-            if num:
-                numbers.append(int(num))
+# Populate grids
+for i, row in enumerate(grid):
+    for j, val in enumerate(row):
+        if val == '.':
+            continue
+        elif val.isdigit():
+            mark_adjacent_cells(number_grid, i, j)
+        else:
+            mark_adjacent_cells(symbol_grid, i, j)
