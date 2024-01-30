a891c93a9c600342ea040c51f75799c912f862f6
Owen Thurston <thurston.owen1@gmail.com>
2024-01-29 23:09:36 -0800

    Completed idea for part 2

--- day3.py
+++ day3.py
@@ -49,15 +49,17 @@ for i, row in enumerate(grid):
 
 print(sum(numbers))
 
-# Part 2
+################### Part 2 #######################
 # Find '*' symbols with 2 numbers adjacent to them
 def num_adjacent(grid, x, y):
+    """ Returns number of separate numbers adjacent to provided coordinate"""
     num_adj = 0
 
     for i in (-1, 0, 1):
+        # last_adj ensures sequential digits aren't counted separately
         last_adj = False
-        for i in (-1, 0, 1):
-            if grid[x][y].isdigit() and not last_adj:
+        for j in (-1, 0, 1):
+            if grid[x + i][y + j].isdigit() and not last_adj:
                 num_adj += 1
                 last_adj = True
             else:
@@ -65,10 +67,44 @@ def num_adjacent(grid, x, y):
 
     return num_adj
 
+def get_gear_ratio(grid, x, y):
+    num1 = ''
+    num2 = ''
+
+    for i in (-1, 0, 1):
+        # last_digit indicates if the previous loop iteration char was a digit
+        last_digit = False
+
+        for j in (-1, 0, 1):
+            if grid[x + i][y + j].isdigit():
+                num1 = num1 + grid[x + i][y + j]
+                # Collect digits going left but not past list limits
+                if not last_digit:
+                    k = j - 1
+                    while grid[x + i][y + k].isdigit() and y + k > 0:
+                        num1 = grid[x + i][y + k] + num1
+                        k -= 1
+                elif j == 1:
+                    # Collect digits going right until a gap reached and not past list limits
+                    k = j + 1
+                    try:
+                        while grid[x + i][y + k].isdigit():
+                            num1 = num1 + grid[x + i][y + k]
+                            k += 1
+                    except IndexError:
+                        # End of row reached
+                        pass
+                last_digit = True
+            else:
+                last_digit = False
+                num2 = num1
+                num1 = ''
+
 gear_ratios = []
 
 for i, row in enumerate(grid):
     for j, val in enumerate(row):
         if val == '*' and num_adjacent(grid, i, j) == 2:
             # This is a gear. Get gear ratio
-            
+            gear_ratios.append(get_gear_ratio(grid, i, j))
+print(sum(gear_ratios))
