7bcba65d94eae441105285afa2ed1b4e03950920
Owen Thurston <thurston.owen1@gmail.com>
2024-01-30 12:54:39 -0800

    I sure hope there's a better way to do this than the way I did, but I got the correct answer.

--- day3.py
+++ day3.py
@@ -59,9 +59,10 @@ def num_adjacent(grid, x, y):
         # last_adj ensures sequential digits aren't counted separately
         last_adj = False
         for j in (-1, 0, 1):
-            if grid[x + i][y + j].isdigit() and not last_adj:
-                num_adj += 1
-                last_adj = True
+            if grid[x + i][y + j].isdigit():
+                if not last_adj:
+                    num_adj += 1
+                    last_adj = True
             else:
                 last_adj = False
 
@@ -73,12 +74,26 @@ def get_gear_ratio(grid, x, y):
 
     for i in (-1, 0, 1):
         # last_digit indicates if the previous loop iteration char was a digit
+        if num1 and not num2:
+            last_digit = False
+            num2 = num1
+            num1 = ''
         last_digit = False
 
         for j in (-1, 0, 1):
             if grid[x + i][y + j].isdigit():
+                # TODO: right guarded by j=1 and left guarded by j=-1
+                # TODO: last_digit determines if the 3x3 numbers are connected
                 num1 = num1 + grid[x + i][y + j]
-                if j == 1:
+
+                if j == -1:
+                    # Collect digits going left but not past list limits
+                    # As well as collect digits in the 3x3 space around the "*"
+                    k = j - 1
+                    while y + k >= 0 and grid[x + i][y + k].isdigit():
+                        num1 = grid[x + i][y + k] + num1
+                        k -= 1
+                elif j == 1:
                     # Collect digits going right until a gap reached and not past list limits
                     k = j + 1
                     try:
@@ -88,24 +103,21 @@ def get_gear_ratio(grid, x, y):
                     except IndexError:
                         # End of row reached
                         pass
-                elif not last_digit:
-                # Collect digits going left but not past list limits
-                # As well as collect digits in the 3x3 space around the "*"
-                    k = j - 1
-                    while grid[x + i][y + k].isdigit() and y + k >= 0:
-                        num1 = grid[x + i][y + k] + num1
-                        k -= 1
                 last_digit = True
             elif num1 and not num2:
                 last_digit = False
                 num2 = num1
                 num1 = ''
+    
     return int(num1) * int(num2)
 
 gear_ratios = []
 
 for i, row in enumerate(grid):
     for j, val in enumerate(row):
+        # DEBUG
+        # if i == 14 and j == 70:
+        #     pass
         if val == '*' and num_adjacent(grid, i, j) == 2:
             # DEBUG
             print(f"({i}, {j})")
